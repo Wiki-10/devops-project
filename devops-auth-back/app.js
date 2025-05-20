8e3160335cc73bd9ddd4fb91cdc100d20a195775
Alejandro Hernandez <ahernandezalvarez@deloitte.com>
2025-05-20 13:34:16 -0600

    Added app.js with basic back functionality

---
+++ devops-auth-back/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+      
