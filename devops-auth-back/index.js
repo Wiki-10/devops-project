19dd07d5efb6821a761be0b4d68d393d5a03f2fc
Alejandro Hernandez <ahernandezalvarez@deloitte.com>
2025-06-11 13:13:27 -0600

    Fixed issue preventing file to acces environment variables

--- devops-auth-back/index.js
+++ devops-auth-back/index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const app = require("./app");
 
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const port = process.env.PORT || 8000;
 
--- devops-auth-back/middleware/authentication.js
+++ devops-auth-back/middleware/authentication.js
@@ -5,10 +5,15 @@ const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
+
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.status(401).send("Unauthorized");
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(401).send("Unauthorized");
+    if (err) {
+      return res.status(401).send("Unauthorized");
+    }
     req.user = user;
     next();
   });
